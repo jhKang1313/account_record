b115c07df57757a278f2a4184890a67d2c622843
jhKang1313 <jinhuk1313@gmail.com>
2015-12-22 23:43:10 +0900

    가계부 프로그램 작성 시작

---
+++ for_me_to_do/src/for_me_to_do/ConnectionMaker.java
@@ -0,0 +1,7 @@
+package for_me_to_do;
+
+import com.mysql.jdbc.Connection;
+
+public interface ConnectionMaker {
+	public Connection getConnection();
+}
---
+++ for_me_to_do/src/for_me_to_do/Main.java
@@ -0,0 +1,7 @@
+package for_me_to_do;
+
+public class Main {
+	public static void main(String[] args){
+		
+	}
+}
